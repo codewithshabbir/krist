fc46c79b3560632613f756e1955e49411c294290
Muhammad Shabbir <110288732+muhammadshabbir07@users.noreply.github.com>
2025-01-10 02:31:02 +0500

    categories skaleton add

--- src/app/components/Sections/Categories/Categories.tsx
+++ src/app/components/Sections/Categories/Categories.tsx
@@ -9,11 +9,11 @@ import classnames from "classnames";
 import { FaArrowRightLong, FaArrowLeftLong } from "react-icons/fa6";
 import styles from "@/app/components/Buttons/ButtonLight.module.css";
 import { CategoryItem } from "@/@types/types";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const Categories = () => {
-  const sliderRef = useRef<Slider | null>(null); 
+  const sliderRef = useRef<Slider | null>(null);
   const [categoryList, setCategoryList] = useState<CategoryItem[]>([]);
-  // const [firstProductByCategory, setfirstProductByCategory] = useState<Product[]>([])
 
   useEffect(() => {
     const fetchData = async () => {
@@ -21,24 +21,12 @@ const Categories = () => {
         const categories = await fetchProductByCategoryList();
         setCategoryList(categories);
         console.log(categories);
-        
       } catch (error) {
         console.error("Error fetching categories:", error);
       }
     };
     fetchData();
   }, []);
-
-  // useEffect(()=>{
-  //   categoryList.map( async (category)=>{
-  //     fetchProductsByCategory(category.slug);
-  //     const categoryfetch  = await fetchProductsByCategory(category.slug);
-  //     setfirstProductByCategory(categoryfetch.products)
-  //   })
-  // })
-
-  // console.log(firstProductByCategory[0]);
-  
 
   // Slider settings
   const settings = {
@@ -88,31 +76,40 @@ const Categories = () => {
       {/* Slider */}
       <div className="pt-5">
         <Slider {...settings} ref={sliderRef}>
-          {categoryList.map((category) => (
-            <div key={category.id} className="px-2">
-              <div className="bg-gray-100 flex items-center w-full rounded-lg relative overflow-hidden h-[300px]">
-                <h3 className="absolute left-6 top-1 text-gray-200 text-6xl font-bold whitespace-nowrap">
-                  {category.name}
-                </h3>
-                <Image
-                  className="relative z-[1] h-[300px]"
-                  src={`${category.imageUrl}`}
-                  alt={category.name}
-                  width={500}
-                  height={100}
-                  layout="responsive"
-                />
-                <div className="absolute bottom-3 z-10 w-4/5 left-[1.7rem]">
-                  <div className={classnames(styles.buttonLight)}>
-                    <div className={classnames(styles.animLayer)}></div>
-                    <a href='#' className={classnames(styles.buttonLightAnchor)}>
-                      All Productst
-                    </a>
+          {categoryList && categoryList.length > 0
+            ? categoryList.map((category) => (
+                <div key={category.id} className="px-2">
+                  <div className="bg-gray-100 flex items-center w-full rounded-lg relative overflow-hidden h-[300px]">
+                    <h3 className="absolute left-6 top-1 text-gray-200 text-6xl font-bold whitespace-nowrap">
+                      {category.name}
+                    </h3>
+                    <Image
+                      className="relative z-[1] h-[300px]"
+                      src={`${category.imageUrl}`}
+                      alt={category.name}
+                      width={500}
+                      height={100}
+                      layout="responsive"
+                    />
+                    <div className="absolute bottom-3 z-10 w-4/5 left-[1.7rem]">
+                      <div className={classnames(styles.buttonLight)}>
+                        <div className={classnames(styles.animLayer)}></div>
+                        <a
+                          href="/shop"
+                          className={classnames(styles.buttonLightAnchor)}
+                        >
+                          Show All
+                        </a>
+                      </div>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </div>
-          ))}
+              ))
+            : [...Array(4)].map((_, index) => (
+                <div key={index} className="px-2">
+                  <Skeleton className="h-[300px] w-full rounded-lg" />
+                </div>
+              ))}
         </Slider>
       </div>
     </div>
